683efb5bd7397f89eb0b875c0319f6eb3a0fb30e
Adam Bien <abien@adam-bien.com>
2015-10-13 11:21:20 +0200

    Pull command execution fixed

--- src/main/java/com/airhacks/airfield/TakeDown.java
+++ src/main/java/com/airhacks/airfield/TakeDown.java
@@ -4,7 +4,11 @@ package com.airhacks.airfield;
 
 import java.io.File;
 import java.io.IOException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import org.eclipse.jgit.api.Git;
+import org.eclipse.jgit.api.PullCommand;
+import org.eclipse.jgit.api.PullResult;
 import org.eclipse.jgit.api.ResetCommand;
 import org.eclipse.jgit.api.errors.GitAPIException;
 
@@ -39,12 +43,22 @@ public class TakeDown {
     void update() {
         try {
             this.git.reset().setMode(ResetCommand.ResetType.HARD).call();
+
             System.out.println("+Changed files removed");
         } catch (GitAPIException ex) {
             throw new IllegalStateException("Cannot reset local repository", ex);
         }
-        this.git.pull();
-        System.out.println("+Files updated, ready to start!");
+        PullCommand command = this.git.pull();
+        try {
+            PullResult pullResult = command.call();
+            if (pullResult.isSuccessful()) {
+                System.out.println("+Files updated, ready to start!");
+            } else {
+                System.out.println("--Download was not successful " + pullResult.toString());
+            }
+        } catch (GitAPIException ex) {
+            Logger.getLogger(TakeDown.class.getName()).log(Level.SEVERE, null, ex);
+        }
     }
 
     boolean openLocal() {
